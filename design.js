baa18a2941cf0e070dca0e46cb21e6ca4592fe44
oKay <learnerkay@gmail.com>
2018-06-30 01:34:39 +0300

    Update design.js

--- design.js
+++ design.js
@@ -1,5 +1,6 @@
 var height, width, color;
 
+// function called on click of submission bbutton
 
 $("#sizePicker").submit(function (event) {
 event.preventDefault();
@@ -8,7 +9,9 @@ width = $('#inputWeight').val();
 makeGrid(height, width);
 });
 
-// grid function to creat table row and data
+// grid function to create table row and data based on the Height input
+// this is most likely going to create square canvases!
+
 function makeGrid (h, w){ 
     $('tr').remove();
 for(var i = 1; i <= h; i++){
@@ -20,7 +23,7 @@ for(var i = 1; i <= h; i++){
     
 }
     
-    //Adding color to cell
+    //function to add color/remove color of a clicked cell.
 $('td').click(function addColor(){
         color = $('#colorPicker').val();
         
